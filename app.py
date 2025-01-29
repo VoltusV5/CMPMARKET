f2fe733c57416a621a9002f25ba6088b6f51581b
Vagym12 <vady835@gmail.com>
2025-01-29 14:13:35 +0300

    Связь между фронтендом и бэкендом(база данных будет позже)

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+
+@app.route("/sign_in")
+def sign_in():
+    return render_template("sign_in.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
