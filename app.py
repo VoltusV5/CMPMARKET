14e0ec7dcc949ea51136c32b30675d80068bcfb5
Vagym12 <vady835@gmail.com>
2025-01-30 12:13:38 +0300

    Неработающая работа с базой данных

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mail = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    nick = db.Column(db.String, nullable=False)
 
 
 @app.route("/")
@@ -14,9 +24,21 @@ def account():
     return render_template("account.html")
 
 
-@app.route("/registration")
+@app.route("/registration", methods=['POST', 'GET'])
 def registration():
-    return render_template("registration.html")
+    if request.method == 'POST':
+        nick = request.form['nick']
+        mail = request.form['mail']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        login = Login(mail=mail, password=password1, nick=nick)
+
+        db.session.add(login)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template("registration.html")
 
 
 @app.route("/sign_in")
