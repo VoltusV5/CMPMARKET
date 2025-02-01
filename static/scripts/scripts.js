84bf94556a401ca55fae0d2a3a73c3688acea1ff
VoltusV5 <maxt.spb@gmail.com>
2025-02-01 14:24:16 +0300

    Сделал подгрузку всех маркетплейсов

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -56,4 +56,60 @@ $(document).ready(function() {
 });
 
 
+/* Подгрузка товаров AliExpress */
+
+$(document).ready(function() {
+    $.getJSON('/parser', function(data) {
+        let container = $('#product-cards-container-AliExpress');
+        container.empty();
+        data.forEach(product => {
+            let card = `
+                <div class="main-page__card">
+                    <a href="${product.pruduct_url}" target="_blank">
+                        <img src="${product.product_photo}" alt="Картинка продукта">
+                        <h1 id="product_cost">${product.product_ozon_card_price}</h1>
+                        <h3 id="product_name">${product.product_name}</h3>
+                        <div class="product_reviews">
+                            <span>🌟${product.product_stars}</span>   
+                            <span>🗯️ ${product.product_reviews}</span>
+                        </div>
+                        <div class="product_date">
+                            <h5>🛒  Завтра</h5>
+                        </div>
+                    </a>
+                </div>
+            `;
+            container.append(card);
+        });
+    });
+});
+
+
+/* Подгрузка товаров WildBerries */
+
+$(document).ready(function() {
+    $.getJSON('/parser', function(data) {
+        let container = $('#product-cards-container-WildBerries');
+        container.empty();
+        data.forEach(product => {
+            let card = `
+                <div class="main-page__card">
+                    <a href="${product.pruduct_url}" target="_blank">
+                        <img src="${product.product_photo}" alt="Картинка продукта">
+                        <h1 id="product_cost">${product.product_ozon_card_price}</h1>
+                        <h3 id="product_name">${product.product_name}</h3>
+                        <div class="product_reviews">
+                            <span>🌟${product.product_stars}</span>   
+                            <span>🗯️ ${product.product_reviews}</span>
+                        </div>
+                        <div class="product_date">
+                            <h5>🛒  Завтра</h5>
+                        </div>
+                    </a>
+                </div>
+            `;
+            container.append(card);
+        });
+    });
+});
 
