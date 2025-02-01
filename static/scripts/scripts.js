f79b4bef1208b48d8af6ade216428d07fe5e15ca
VoltusV5 <maxt.spb@gmail.com>
2025-02-01 14:12:10 +0300

    добавил YandexMarket

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -2,21 +2,52 @@
 
 $(document).ready(function() {
     $.getJSON('/parser', function(data) {
-        let container = $('#product-cards-container');
+        let container = $('#product-cards-container-ozon');
         container.empty();
         data.forEach(product => {
             let card = `
                 <div class="main-page__card">
-                    <a href="${product.pruduct_url}" target="_blank"><img src="${product.product_photo}" alt="Картинка продукта"></a>
-                    <h1 id="product_cost">${product.product_ozon_card_price}</h1>
-                    <h3 id="product_name">${product.product_name}</h3>
-                    <div class="product_reviews">
-                        <span>🌟${product.product_stars}</span>   
-                        <span>🗯️ ${product.product_reviews}</span>
-                    </div>
-                    <div class="product_date">
-                        <h5>🛒  Завтра</h5>
-                    </div>
+                    <a href="${product.pruduct_url}" target="_blank">
+                        <img src="${product.product_photo}" alt="Картинка продукта">
+                        <h1 id="product_cost">${product.product_ozon_card_price}</h1>
+                        <h3 id="product_name">${product.product_name}</h3>
+                        <div class="product_reviews">
+                            <span>🌟${product.product_stars}</span>   
+                            <span>🗯️ ${product.product_reviews}</span>
+                        </div>
+                        <div class="product_date">
+                            <h5>🛒  Завтра</h5>
+                        </div>
+                    </a>
+                </div>
+            `;
+            container.append(card);
+        });
+    });
+});
+
+
+/* Подгрузка товаров YandexMarket */
+
+$(document).ready(function() {
+    $.getJSON('/parser', function(data) {
+        let container = $('#product-cards-container-yandex-market');
+        container.empty();
+        data.forEach(product => {
+            let card = `
+                <div class="main-page__card">
+                    <a href="${product.pruduct_url}" target="_blank">
+                        <img src="${product.product_photo}" alt="Картинка продукта">
+                        <h1 id="product_cost">${product.product_ozon_card_price}</h1>
+                        <h3 id="product_name">${product.product_name}</h3>
+                        <div class="product_reviews">
+                            <span>🌟${product.product_stars}</span>   
+                            <span>🗯️ ${product.product_reviews}</span>
+                        </div>
+                        <div class="product_date">
+                            <h5>🛒  Завтра</h5>
+                        </div>
+                    </a>
                 </div>
             `;
             container.append(card);
