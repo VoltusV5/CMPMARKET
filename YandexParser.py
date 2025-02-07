e70a2faf1609944d558cc74a90141338223b06ce
VoltusV5 <maxt.spb@gmail.com>
2025-02-07 12:42:00 +0300

    yandex2

--- YandexParser.py
+++ YandexParser.py
@@ -18,7 +18,7 @@ def sort(products):
     s = []
     for i in range(4):
         s.append(onion[i][1])
-    with open("BeautifulOzonProducts.json", 'w', encoding="UTF-8") as file:
+    with open("BeautifulYandexProducts.json", 'w', encoding="UTF-8") as file:
         json.dump(s,file,indent=4,ensure_ascii=False)
 
 
